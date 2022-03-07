f19adaf3d1729ce0cb751f9d6a7d487afda099c1
ubatalov <umarbatalov@gmail.com>
2022-03-07 23:56:57 +0300

    champagne-tower

--- main.go
+++ main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"fmt"
-	"leetcode/problems/easy"
+	"leetcode/problems/medium"
 )
 
 func main() {
 	// 0.18750
-	fmt.Println(easy.ChampagneTower(25, 6, 1))
+	fmt.Println(medium.ChampagneTower(25, 6, 1))
 	//fmt.Println(easy.ChampagneTower2(100000009, 33, 17))
 	//fmt.Println(easy.ChampagneTower(10, 3, 1))
 
